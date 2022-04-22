62be3993990ca0768919362492ba562fe82de6a4
Raymond Shiner <rshiner@A-1087.local>
2022-04-22 10:29:34 -0700

    add nvmrc version

--- components/Navigation/Navigation.vue
+++ components/Navigation/Navigation.vue
@@ -4,31 +4,34 @@
     :class="{ isOpen: menuOpen }"
     ref="menu"
   >
-    <NuxtLink
-      v-for="link in navLinks"
-      :key="link.label"
-      :to="link.path"
-      class="block py-4 px-4 text-sm font-medium tracking-wide uppercase border-b-[1px] md:flex md:py-2 md:px-2 md:border-b-0"
-    >
-      {{ link.label }}
-    </NuxtLink>
+    <Prefetch>
+      <NuxtLink
+        v-for="link in navLinks"
+        :key="link.label"
+        :to="link.path"
+        class="block py-4 px-4 text-sm font-medium tracking-wide uppercase border-b-[1px] md:flex md:py-2 md:px-2 md:border-b-0"
+      >
+        {{ link.label }}
+      </NuxtLink>
+    </Prefetch>
   </nav>
 </template>
 
 <script setup>
-import { navLinks } from "~/constants";
-import { useShopStore } from "~/stores/shop";
-import { breakpointsTailwind, useBreakpoints } from "@vueuse/core";
+import { navLinks } from "~/constants"
+import { useShopStore } from "~/stores/shop"
+import { breakpointsTailwind, useBreakpoints } from "@vueuse/core"
+import Prefetch from "@layer0/vue/Prefetch"
 
-const shopStore = useShopStore();
-const menuOpen = computed(() => shopStore.menuOpen);
+const shopStore = useShopStore()
+const menuOpen = computed(() => shopStore.menuOpen)
 
-const route = useRoute();
-const breakpoints = useBreakpoints(breakpointsTailwind);
-const isMobileUI = breakpoints.smaller("md");
-const menu = ref(null);
+const route = useRoute()
+const breakpoints = useBreakpoints(breakpointsTailwind)
+const isMobileUI = breakpoints.smaller("md")
+const menu = ref(null)
 
-watch(route, () => (isMobileUI ? shopStore.toggleMenu(false) : null));
+watch(route, () => (isMobileUI ? shopStore.toggleMenu(false) : null))
 </script>
 
 <style lang="postcss">
